05cc74e17b5e06b2898f0967c937b23a2f5f3dac
gcgus <gcgus.gc@gmail.com>
2021-06-03 11:50:50 +0200

    Coches añadidos

--- src/Car.cpp
+++ src/Car.cpp
@@ -9,7 +9,7 @@ Car::Car()
 	//Cargar la textura(ahora mismo es temporal, la textura difiere segun el coche
 	this->texture= Texture::Get("data/Cars/Car_03.tga");
 	//Lo mismo con el mesh
-	this->mesh = Mesh::Get("data/Cars/car1.obj");
+	this->mesh = Mesh::Get("data/Cars/car10.obj");
 	//
 	shader = Shader::Get("data/shaders/basic.vs", "data/shaders/texture.fs");
 
